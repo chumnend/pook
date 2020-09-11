c938061aeebcf56f89d61ed72e01e674d47c7642
Nicholas Chumney <nchumney@hotmail.com>
2020-09-11 18:46:13 -0400

    setup mongoDB using mongoose

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -5,4 +5,7 @@ require('dotenv').config();
 module.exports = {
   env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 8080,
+  db: (process.env.NODE_ENV = 'test'
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL),
 };
