1ddd83829d9d56de8a387f2362a9e07c8aa4b1c5
Nicholas Chumney <nchumney@hotmail.com>
2020-09-15 11:32:39 -0400

    Update index.js
    fixed db config assignment

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -6,7 +6,7 @@ module.exports = {
   env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 8080,
   secret: process.env.SECRET || 'shhh',
-  db: (process.env.NODE_ENV = 'test'
+  db: (process.env.NODE_ENV === 'test'
     ? process.env.TEST_DATABASE_URL
     : process.env.DATABASE_URL),
 };
