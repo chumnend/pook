cc2b5893ba71fda19efd4040e7ad711c3e0c9656
Nicholas Chumney <nicholas.chumney@outlook.com>
2020-09-27 15:29:38 -0400

    added defaults to config file

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -4,10 +4,10 @@ require('dotenv').config();
 
 module.exports = {
   env: process.env.NODE_ENV || 'development',
-  port: process.env.PORT || 8080,
+  port: process.env.PORT || 3001,
   secret: process.env.SECRET || 'shhh',
   db:
     process.env.NODE_ENV === 'test'
-      ? process.env.TEST_DATABASE_URL
-      : process.env.DATABASE_URL,
+      ? process.env.TEST_DATABASE_URL || 'mongodb://localhost/hotelio_test'
+      : process.env.DATABASE_URL || 'mongodb://localhost/hotelio_dev',
 };
