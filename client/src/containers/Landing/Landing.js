87e5fb31570d55228c8c7e96388895c50db56d1c
Nicholas Chumney <nicholas.chumney@outlook.com>
2020-10-22 09:23:49 -0400

    started laying out landing page

--- client/src/containers/Landing/Landing.js
+++ client/src/containers/Landing/Landing.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import { Page } from './styles';
+import { Page, HeroImage, HeroText } from './styles';
 
 function Landing() {
   return (
     <Page>
-      <h1>Landing Page</h1>
+      <HeroImage>
+        <HeroText>
+          <p>Hotelio</p>
+          <h1>Enjoy a luxury experience</h1>
+          <button>Book Now</button>
+        </HeroText>
+      </HeroImage>
+
+      <div>Enjoy a luxury experience</div>
+      <div>Heres some marketing about us</div>
+      <div>Featured booking experience</div>
+      <div>Sample Rooms you can book</div>
     </Page>
   );
 }
