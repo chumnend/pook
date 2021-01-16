347788f51c314e885bb30c00d6373482fb3d2707
Nicholas Chumney <nicholas.chumney@outlook.com>
2021-01-15 19:28:46 -0500

    setup cors middleware

--- internal/app.go
+++ internal/app.go
@@ -30,6 +30,7 @@ func (app *App) Init(connectionString string) {
 
 	// setup routes
 	app.Router = mux.NewRouter().StrictSlash(true)
+	app.Router.Use(cors)
 	app.Router.HandleFunc("/", pingServer).Methods("GET")
 }
 
@@ -37,6 +38,17 @@ func (app *App) Init(connectionString string) {
 func (app *App) Run(addr string) {
 	log.Printf("listening on %s\n", addr)
 	log.Fatal(http.ListenAndServe(addr, app.Router))
+}
+
+// Middleware ===================================
+func cors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+		next.ServeHTTP(w, r)
+	})
 }
 
 // Handlers =====================================
