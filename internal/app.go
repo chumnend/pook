c67629d2838cc1461616d890d1ce8233912e4449
Nicholas Chumney <nicholas.chumney@outlook.com>
2021-01-16 16:07:10 -0500

    split cors into seperate file

--- internal/app.go
+++ internal/app.go
@@ -49,17 +49,6 @@ func (app *App) Run(addr string) {
 	log.Fatal(http.ListenAndServe(addr, app.Router))
 }
 
-// Middleware ===================================
-func cors(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-
-		next.ServeHTTP(w, r)
-	})
-}
-
 // Handlers =====================================
 func (app *App) pingServer(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Ready to serve requests")
