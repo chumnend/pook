e87f9f5704ecf3d99c521d58dbded39e0236da47
Nicholas Chumney <nicholas.chumney@outlook.com>
2021-03-23 00:04:49 -0400

    setup spa handler to serve react files

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+
+	"github.com/chumnend/pook/internal/pook"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	// load environment variables
+	err := godotenv.Load()
+	if err != nil {
+		log.Println(".env file not found")
+	}
+
+	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL == "" {
+		log.Fatal("missing env: DATABASE_URL")
+	}
+
+	secret := os.Getenv("SECRET_KEY")
+	if secret == "" {
+		log.Fatal("missing env: SECRET_KEY")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("missing env: DATABASE_URL")
+	}
+
+	s := pook.NewServer()
+	s.Serve(":" + port)
 }
--- main_test.go
+++ main_test.go
@@ -1,12 +1,68 @@
 package main
 
 import (
+	"log"
+	"net/http"
+	"net/http/httptest"
 	"os"
+	"strings"
 	"testing"
+
+	"github.com/chumnend/pook/internal/pook"
+	"github.com/joho/godotenv"
 )
 
+var s *pook.Server
+
 func TestMain(m *testing.M) {
+	// load environment variables
+	err := godotenv.Load()
+	if err != nil {
+		log.Println(".env file not found")
+	}
+
+	dbURL := os.Getenv("DATABASE_TEST_URL")
+	if dbURL == "" {
+		log.Fatal("missing env: DATABASE_URL")
+	}
+
+	secret := os.Getenv("SECRET_KEY")
+	if secret == "" {
+		log.Fatal("missing env: SECRET_KEY")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("missing env: DATABASE_URL")
+	}
+
+	s = pook.NewServer()
+
 	// start test runner
 	code := m.Run()
 	os.Exit(code)
+}
+
+func executeRequest(request *http.Request) *httptest.ResponseRecorder {
+	rr := httptest.NewRecorder()
+	s.Router.ServeHTTP(rr, request)
+
+	return rr
+}
+
+func checkResponseCode(t *testing.T, expected, actual int) {
+	if expected != actual {
+		t.Errorf("Expected response code %d. Got %d\n.", expected, actual)
+	}
+}
+
+func TestSpaHandler(t *testing.T) {
+	request, _ := http.NewRequest("GET", "/", nil)
+	res := executeRequest(request)
+
+	checkResponseCode(t, http.StatusOK, res.Code)
+
+	if body := res.Body.String(); !strings.Contains(body, "doctype html") {
+		t.Errorf("Expected string to contain html. Got %s", body)
+	}
 }
