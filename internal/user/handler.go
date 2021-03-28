08c33a25542d67952587186afcc47f8ab00a6a30
Nicholas Chumney <nicholas.chumney@outlook.com>
2021-03-28 13:39:12 -0400

    added functionality to auth routes (/register and /login) and basic tests

--- internal/user/handler.go
+++ internal/user/handler.go
@@ -1,6 +1,7 @@
 package user
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 
@@ -29,14 +30,57 @@ func AttachHandler(r *mux.Router, db *gorm.DB) {
 
 // Register creates a new user and returns jwt token
 func (h *Handler) Register(w http.ResponseWriter, r *http.Request) {
-	log.Println("POST - register")
-	utils.RespondWithError(w, http.StatusNotImplemented, "not yet implemented")
+	// create new user struct
+	var u User
+	if err := json.NewDecoder(r.Body).Decode(&u); err != nil {
+		utils.RespondWithError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+	defer r.Body.Close()
+
+	// call method to create user in DB
+	if err := u.Create(h.DB); err != nil {
+		utils.RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	// generate jwt token
+	if token, err := u.GenerateToken(); err != nil {
+		utils.RespondWithError(w, http.StatusInternalServerError, err.Error())
+	} else {
+		utils.RespondWithJSON(w, http.StatusOK, map[string]string{"token": token})
+	}
 }
 
 // Login validates credentials and returns jwt token if valid
 func (h *Handler) Login(w http.ResponseWriter, r *http.Request) {
-	log.Println("POST - login")
-	utils.RespondWithError(w, http.StatusNotImplemented, "not yet implemented")
+	// get credentials from request
+	var creds User
+	if err := json.NewDecoder(r.Body).Decode(&creds); err != nil {
+		utils.RespondWithError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+	defer r.Body.Close()
+
+	// check to see if user exists
+	u, err := FindUserByEmail(h.DB, creds.Email)
+	if err != nil {
+		utils.RespondWithError(w, http.StatusBadRequest, "Invalid Email and/or Password")
+		return
+	}
+
+	// compare password with found users
+	isValid := u.comparePassword(creds.Password)
+	if !isValid {
+		utils.RespondWithError(w, http.StatusBadRequest, "Invalid Email and/or Password")
+	}
+
+	// generate jwt token
+	if token, err := u.GenerateToken(); err != nil {
+		utils.RespondWithError(w, http.StatusInternalServerError, err.Error())
+	} else {
+		utils.RespondWithJSON(w, http.StatusOK, map[string]string{"token": token})
+	}
 }
 
 // ListUsers returns list of Users found in the DB
--- internal/user/model.go
+++ internal/user/model.go
@@ -2,17 +2,20 @@ package user
 
 import (
 	"errors"
+	"os"
 	"time"
 
 	"github.com/chumnend/pook/internal/book"
 	"github.com/chumnend/pook/internal/task"
+	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/jinzhu/gorm"
+	"golang.org/x/crypto/bcrypt"
 )
 
 // User struct declaration
 type User struct {
-	ID        uint      `gorm:"primaryKey"`
-	Email     string    `gorm:"unique;not null" json:"id"`
+	ID        uint      `gorm:"primaryKey" json:"id"`
+	Email     string    `gorm:"unique;not null" json:"email"`
 	Password  string    `gorm:"unique;not null" json:"password"`
 	Firstname string    `json:"firstname"`
 	Lastname  string    `json:"lastname"`
@@ -31,7 +34,14 @@ func NewUser() *User {
 
 // Create adds a User to the DB
 func (u *User) Create(db *gorm.DB) error {
-	return errors.New("Not implemented")
+	// hash the password
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	u.Password = string(hashedPassword)
+
+	return db.Create(&u).Error
 }
 
 // Update updates the User in the DB
@@ -42,6 +52,39 @@ func (u *User) Update(db *gorm.DB) error {
 // Delete dletes the User in the DB
 func (u *User) Delete(db *gorm.DB) error {
 	return errors.New("Not implemented")
+}
+
+// Token struct declaration
+type Token struct {
+	ID    uint
+	Email string
+	*jwt.StandardClaims
+}
+
+// GenerateToken creates jwt token using User internal data
+func (u *User) GenerateToken() (string, error) {
+	tk := Token{
+		ID:             u.ID,
+		Email:          u.Email,
+		StandardClaims: &jwt.StandardClaims{},
+	}
+
+	token := jwt.NewWithClaims(jwt.GetSigningMethod("HS256"), tk)
+	tokenStr, err := token.SignedString([]byte(os.Getenv("SECRET_KEY")))
+	if err != nil {
+		return "", err
+	}
+
+	return tokenStr, nil
+}
+
+func (u *User) comparePassword(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
+	if err != nil {
+		return false
+	}
+
+	return true
 }
 
 // ListUsers returns a list of users in the DB
@@ -57,6 +100,11 @@ func FindUserByID(db *gorm.DB, id string) (User, error) {
 
 // FindUserByEmail takes an ID and returns a user struct
 func FindUserByEmail(db *gorm.DB, email string) (User, error) {
-	u := User{}
+	var u User
+	err := db.Where("email = ?", email).First(&u).Error
+	if err != nil {
+		return u, err
+	}
+
 	return u, nil
 }
