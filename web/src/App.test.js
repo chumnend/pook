268d02fe5256cdb303d74367663241d579803fcf
Nicholas Chumney <nicholas.chumney@outlook.com>
2021-03-28 15:03:39 -0400

    setup react-router-dom with home and notfound pages

--- web/src/App.test.js
+++ web/src/App.test.js
@@ -1,8 +1,26 @@
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders home page', () => {
+  render(
+    <MemoryRouter initialEntries={['/']}>
+      <App />
+    </MemoryRouter>,
+  );
+
+  const el = screen.getByText(/Pook/i);
+  expect(el).toBeInTheDocument();
 });
+
+test('renders home page', () => {
+  render(
+    <MemoryRouter initialEntries={['/not-found']}>
+      <App />
+    </MemoryRouter>,
+  );
+
+  const el = screen.getByText(/Page not found/i);
+  expect(el).toBeInTheDocument();
+});
+
---
+++ web/src/pages/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>Pook: Your Personal Planning Book</h1>
+    </div>
+  );
+};
+
+export default Home;
