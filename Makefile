f60953c337688e1654b6adeaef61249a79058b12
Nicholas Chumney <nicholas.chumney@outlook.com>
2021-04-02 16:01:30 -0400

    cleaned up tests

--- Makefile
+++ Makefile
@@ -5,24 +5,28 @@ build: build-react build-go
 
 .PHONY: build-react
 build-react:
-	@echo "Building..."
 	@if [ ! -d "web/node_modules" ]; then \
   	cd web && npm install; \
 	fi
 	@cd web && npm run build
+	@echo "React assests built."
 
 .PHONY: build-go
 build-go:
 	@mkdir -p bin/
 	@cd bin/ && go build ../main.go
+	@echo "Go assests built."
 
 .PHONY: serve
 serve:
-	@echo "Starting server..."
 	@./bin/main
+
+.PHONY: test
+test:
+	@go test
+	@cd web && npm test
 
 .PHONY: clean
 clean:
-	@echo "Cleaning..."
-	@rm -rf bin web/build
+	@rm -rf bin web/build web/node_modules
 	@echo "Clean complete."
--- web/src/App.test.js
+++ web/src/App.test.js
@@ -1,36 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import App from './App';
+import { AuthProvider } from './context/auth';
 
-test('renders home page', () => {
+test('render <App />', () => {
   render(
-    <MemoryRouter initialEntries={['/home']}>
-      <App />
-    </MemoryRouter>,
-  );
-
-  const el = screen.getByText(/Home/i);
-  expect(el).toBeInTheDocument();
-});
-
-test('renders landing page', () => {
-  render(
-    <MemoryRouter initialEntries={['/']}>
-      <App />
-    </MemoryRouter>,
-  );
+    <AuthProvider>
+      <MemoryRouter initialEntries={['/']}>
+        <App />
+      </MemoryRouter>
+    </AuthProvider>,
+  )
 
   const el = screen.getByText(/Landing/i);
   expect(el).toBeInTheDocument();
-});
-
-test('renders not found page', () => {
-  render(
-    <MemoryRouter initialEntries={['/not-found']}>
-      <App />
-    </MemoryRouter>,
-  );
-
-  const el = screen.getByText(/Page not found/i);
-  expect(el).toBeInTheDocument();
-});
+})
