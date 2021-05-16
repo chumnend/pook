a84ccba3285e5b3eb7c62fcb342b15e86b2e255c
Nicholas Chumney <nicholas.chumney@outlook.com>
2021-05-16 16:57:44 -0400

    lazy loading pages

--- web/src/components/Router/Router.js
+++ web/src/components/Router/Router.js
@@ -1,11 +1,13 @@
 import PropTypes from 'prop-types';
+import React, { Suspense } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import LandingPage from '../LandingPage';
-import NotFoundPage from '../NotFoundPage';
 import ProtectedRoute from '../ProtectedRoute';
-import SignInPage from '../SignInPage';
-import SignUpPage from '../SignUpPage';
+
+const LandingPage = React.lazy(() => import('../LandingPage'));
+const NotFoundPage = React.lazy(() => import('../NotFoundPage'));
+const SignInPage = React.lazy(() => import('../SignInPage'));
+const SignUpPage = React.lazy(() => import('../SignUpPage'));
 
 export const HOME_ROUTE = '/';
 export const REGISTER_ROUTE = '/register';
@@ -17,39 +19,41 @@ const Router = ({ user = null }) => {
   const isSignedIn = user !== null;
 
   return (
-    <BrowserRouter>
-      <Switch>
-        {/** authenticated routes */}
-        <ProtectedRoute
-          path={LOGOUT_ROUTE}
-          component={() => <h1>Register</h1>}
-          condition={isSignedIn}
-          redirect={HOME_ROUTE}
-        />
+    <Suspense fallback={<div>Loading...</div>}>
+      <BrowserRouter>
+        <Switch>
+          {/** authenticated routes */}
+          <ProtectedRoute
+            path={LOGOUT_ROUTE}
+            component={() => <h1>Register</h1>}
+            condition={isSignedIn}
+            redirect={HOME_ROUTE}
+          />
 
-        {/** unauthenticated routes */}
-        <ProtectedRoute
-          path={LOGIN_ROUTE}
-          component={SignInPage}
-          condition={!isSignedIn}
-          redirect={HOME_ROUTE}
-        />
-        <ProtectedRoute
-          path={REGISTER_ROUTE}
-          component={SignUpPage}
-          condition={!isSignedIn}
-          redirect={HOME_ROUTE}
-        />
+          {/** unauthenticated routes */}
+          <ProtectedRoute
+            path={LOGIN_ROUTE}
+            component={SignInPage}
+            condition={!isSignedIn}
+            redirect={HOME_ROUTE}
+          />
+          <ProtectedRoute
+            path={REGISTER_ROUTE}
+            component={SignUpPage}
+            condition={!isSignedIn}
+            redirect={HOME_ROUTE}
+          />
 
-        {/** default routes */}
-        <Route
-          exact
-          path={HOME_ROUTE}
-          component={isSignedIn ? () => <h1>Home</h1> : LandingPage}
-        />
-        <Route component={NotFoundPage} />
-      </Switch>
-    </BrowserRouter>
+          {/** default routes */}
+          <Route
+            exact
+            path={HOME_ROUTE}
+            component={isSignedIn ? () => <h1>Home</h1> : LandingPage}
+          />
+          <Route component={NotFoundPage} />
+        </Switch>
+      </BrowserRouter>
+    </Suspense>
   );
 };
 
