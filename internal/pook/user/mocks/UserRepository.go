12cc55aebc8d4cbc5c376ba6d3412aa534bc30a6
Nicholas Chumney <nicholas.chumney@outlook.com>
2021-06-09 20:48:44 -0400

    wrote tests for UserService

--- internal/pook/user/mocks/UserRepository.go
+++ internal/pook/user/mocks/UserRepository.go
@@ -13,19 +13,17 @@ type UserRepository struct {
 // FindAll provides a mock function
 func (mock *UserRepository) FindAll() ([]domain.User, error) {
 	args := mock.Called()
-	result := args.Get(0)
-	return result.([]domain.User), args.Error(1)
+	return args.Get(0).([]domain.User), args.Error(1)
 }
 
 // FindByEmail provides a mock function
 func (mock *UserRepository) FindByEmail(email string) (*domain.User, error) {
 	args := mock.Called(email)
-	result := args.Get(0)
-	return result.(*domain.User), args.Error(1)
+	return args.Get(0).(*domain.User), args.Error(1)
 }
 
 // Save provides a mock function given a User struct
 func (mock *UserRepository) Save(user *domain.User) error {
 	args := mock.Called(user)
-	return args.Error(1)
+	return args.Error(0)
 }
--- internal/pook/user/service_test.go
+++ internal/pook/user/service_test.go
@@ -1,96 +1,227 @@
 package user
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/mock"
+	"golang.org/x/crypto/bcrypt"
 
 	"github.com/chumnend/pook/internal/pook/domain"
 	"github.com/chumnend/pook/internal/pook/user/mocks"
 	"github.com/stretchr/testify/assert"
 )
 
-// FindAll
 func TestFindAll(t *testing.T) {
-	// setup
 	mockRepo := new(mocks.UserRepository)
-	user := domain.User{Email: "tester@pook.com", Password: "123"}
-	mockRepo.On("FindAll").Return([]domain.User{user}, nil)
-	testService := NewService(mockRepo)
+	mockUser := domain.User{
+		Email:    "tester@pook.com",
+		Password: "123",
+	}
 
-	// run
-	result, _ := testService.FindAll()
+	mockListUsers := make([]domain.User, 0)
+	mockListUsers = append(mockListUsers, mockUser)
 
-	// check
-	mockRepo.AssertExpectations(t)
-	assert.Equal(t, user.Email, result[0].Email)
-	assert.Equal(t, user.Password, result[0].Password)
+	t.Run("success", func(t *testing.T) {
+		// setup
+		mockRepo.On("FindAll").Return(mockListUsers, nil).Once()
+		testService := NewService(mockRepo)
+
+		// run
+		result, err := testService.FindAll()
+
+		// check
+		mockRepo.AssertExpectations(t)
+		assert.Len(t, result, len(mockListUsers))
+		assert.Equal(t, mockUser.Email, result[0].Email)
+		assert.Equal(t, mockUser.Password, result[0].Password)
+		assert.NoError(t, err)
+	})
+
+	t.Run("fail", func(t *testing.T) {
+		// setup
+		mockRepo.On("FindAll").Return([]domain.User{}, errors.New("Unexpected error")).Once()
+		testService := NewService(mockRepo)
+
+		// run
+		result, err := testService.FindAll()
+
+		// check
+		mockRepo.AssertExpectations(t)
+		assert.Len(t, result, 0)
+		assert.Error(t, err)
+	})
 }
 
-// FindByEmail
 func TestFindByEmail(t *testing.T) {
-	// setup
 	mockRepo := new(mocks.UserRepository)
-	user := domain.User{Email: "tester@pook.com", Password: "123"}
-	mockRepo.On("FindByEmail", mock.AnythingOfType("string")).Return(&user, nil)
-	testService := NewService(mockRepo)
+	mockUser := domain.User{
+		Email:    "tester@pook.com",
+		Password: "123",
+	}
 
-	// run
-	result, _ := testService.FindByEmail("tester@pook.com")
+	t.Run("success", func(t *testing.T) {
+		// setup
+		mockRepo.On("FindByEmail", mock.AnythingOfType("string")).Return(&mockUser, nil).Once()
+		testService := NewService(mockRepo)
 
-	// check
-	mockRepo.AssertExpectations(t)
-	assert.Equal(t, user.Email, result.Email)
-	assert.Equal(t, user.Password, result.Password)
+		// run
+		result, err := testService.FindByEmail("tester@pook.com")
+
+		// check
+		mockRepo.AssertExpectations(t)
+		assert.Equal(t, mockUser.Email, result.Email)
+		assert.Equal(t, mockUser.Password, result.Password)
+		assert.NoError(t, err)
+	})
+
+	t.Run("fail", func(t *testing.T) {
+		// setup
+		mockRepo.On("FindByEmail", mock.AnythingOfType("string")).Return(&domain.User{}, errors.New("Unexpected error")).Once()
+		testService := NewService(mockRepo)
+
+		// run
+		result, err := testService.FindByEmail("tester@pook.com")
+
+		// check
+		mockRepo.AssertExpectations(t)
+		assert.Equal(t, "", result.Email)
+		assert.Equal(t, "", result.Password)
+		assert.Error(t, err)
+	})
 }
 
-// Validate
-func TestValidateUser(t *testing.T) {
-	// setup
-	testService := NewService(nil)
-	user := &domain.User{Email: "tester@pook.com", Password: "123"}
+func TestSave(t *testing.T) {
+	mockRepo := new(mocks.UserRepository)
 
-	// run
-	err := testService.Validate(user)
+	t.Run("success", func(t *testing.T) {
+		// setup
+		mockRepo.On("Save", mock.Anything).Return(nil).Once()
+		testService := NewService(mockRepo)
 
-	// check
-	assert.Nil(t, err)
+		// run
+		result := testService.Save(&domain.User{})
+
+		// check
+		mockRepo.AssertExpectations(t)
+		assert.NoError(t, result)
+	})
+
+	t.Run("fail", func(t *testing.T) {
+		// setup
+		mockRepo.On("Save", mock.Anything).Return(errors.New("Unexpected error")).Once()
+		testService := NewService(mockRepo)
+
+		// run
+		result := testService.Save(&domain.User{})
+
+		// check
+		mockRepo.AssertExpectations(t)
+		assert.Error(t, result)
+	})
 }
 
-func TestValidateEmptyUser(t *testing.T) {
-	// setup
-	testService := NewService(nil)
+func TestValidate(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		// setup
+		testService := NewService(nil)
+		mockUser := &domain.User{
+			Email:    "tester@pook.com",
+			Password: "123",
+		}
 
-	// run
-	err := testService.Validate(nil)
+		// run
+		err := testService.Validate(mockUser)
 
-	// check
-	assert.NotNil(t, err)
-	assert.Equal(t, err.Error(), "User is empty")
+		// check
+		assert.Nil(t, err)
+	})
+
+	t.Run("fail - empty user", func(t *testing.T) {
+		// setup
+		testService := NewService(nil)
+
+		// run
+		err := testService.Validate(nil)
+
+		// check
+		assert.NotNil(t, err)
+		assert.Equal(t, err.Error(), "User is empty")
+	})
+
+	t.Run("fail - no email", func(t *testing.T) {
+		// setup
+		testService := NewService(nil)
+		mockUser := &domain.User{
+			Email:    "",
+			Password: "123",
+		}
+
+		// run
+		err := testService.Validate(mockUser)
+
+		// check
+		assert.NotNil(t, err)
+		assert.Equal(t, err.Error(), "Invalid User")
+	})
+
+	t.Run("fail - no password", func(t *testing.T) {
+		// setup
+		testService := NewService(nil)
+		mockUser := &domain.User{
+			Email:    "tester@pook.com",
+			Password: "",
+		}
+
+		// run
+		err := testService.Validate(mockUser)
+
+		// check
+		assert.NotNil(t, err)
+		assert.Equal(t, err.Error(), "Invalid User")
+	})
 }
 
-func TestValidateEmptyEmail(t *testing.T) {
-	// setup
-	testService := NewService(nil)
-	user := &domain.User{Email: "", Password: "123"}
+func TestGenerateToken(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		// setup
+		testService := NewService(nil)
+		mockUser := &domain.User{
+			Email:    "tester@pook.com",
+			Password: "123",
+		}
 
-	// run
-	err := testService.Validate(user)
+		// run
+		token, err := testService.GenerateToken(mockUser)
 
-	// check
-	assert.NotNil(t, err)
-	assert.Equal(t, err.Error(), "Invalid User")
+		// check
+		assert.Greater(t, len(token), 0)
+		assert.NoError(t, err)
+	})
 }
 
-func TestValidateEmptyPassword(t *testing.T) {
-	// setup
+func TestComparePassword(t *testing.T) {
 	testService := NewService(nil)
-	user := &domain.User{Email: "tester@pook.com", Password: ""}
+	password := "123"
+	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	mockUser := &domain.User{
+		Email:    "tester@pook.com",
+		Password: string(hashedPassword),
+	}
 
-	// run
-	err := testService.Validate(user)
+	t.Run("success", func(t *testing.T) {
+		// run
+		err := testService.ComparePassword(mockUser, password)
 
-	// check
-	assert.NotNil(t, err)
-	assert.Equal(t, err.Error(), "Invalid User")
+		// check
+		assert.NoError(t, err)
+	})
+
+	t.Run("fail - wrong password", func(t *testing.T) {
+		// run
+		err := testService.ComparePassword(mockUser, "")
+
+		// check
+		assert.Error(t, err)
+	})
 }
