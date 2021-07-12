812c7fed8de792de77d716c81ddf272952737520
Nicholas Chumney <nicholas.chumney@outlook.com>
2021-07-12 19:45:25 -0400

    clean up service mock

--- internal/user/repository/repository_mock.go
+++ internal/user/repository/repository_mock.go
@@ -10,25 +10,25 @@ type MockUserRepository struct {
 	mock.Mock
 }
 
-// FindAll provides a mock function
+// FindAll mock method
 func (mock *MockUserRepository) FindAll() ([]domain.User, error) {
 	args := mock.Called()
 	return args.Get(0).([]domain.User), args.Error(1)
 }
 
-// FindByEmail provides a mock function
+// FindByEmail mock method
 func (mock *MockUserRepository) FindByEmail(email string) (*domain.User, error) {
 	args := mock.Called(email)
 	return args.Get(0).(*domain.User), args.Error(1)
 }
 
-// Save provides a mock function given a User struct
+// Save mock method given a User struct
 func (mock *MockUserRepository) Save(user *domain.User) error {
 	args := mock.Called(user)
 	return args.Error(0)
 }
 
-// Migrate provides a mock function
+// Migrate mock method
 func (mock *MockUserRepository) Migrate() error {
 	args := mock.Called()
 	return args.Error(0)
--- internal/user/service/service_mock.go
+++ internal/user/service/service_mock.go
@@ -10,37 +10,37 @@ type MockUserService struct {
 	mock.Mock
 }
 
-// FindAll provides mock function
+// FindAll mock method
 func (mock *MockUserService) FindAll() ([]domain.User, error) {
 	args := mock.Called()
 	return args.Get(0).([]domain.User), args.Error(1)
 }
 
-// FindByEmail provides mock function
+// FindByEmail mock method
 func (mock *MockUserService) FindByEmail(email string) (*domain.User, error) {
 	args := mock.Called(email)
 	return args.Get(0).(*domain.User), args.Error(1)
 }
 
-// Save provides mock function
+// Save mock method
 func (mock *MockUserService) Save(user *domain.User) error {
 	args := mock.Called(user)
 	return args.Error(0)
 }
 
-// Validate provides mock function
+// Validate mock method
 func (mock *MockUserService) Validate(user *domain.User) error {
 	args := mock.Called(user)
 	return args.Error(0)
 }
 
-// GenerateToken provides mock function
+// GenerateToken mock method
 func (mock *MockUserService) GenerateToken(user *domain.User) (string, error) {
 	args := mock.Called(user)
 	return args.String(0), args.Error(1)
 }
 
-// ComparePassword provides mock function
+// ComparePassword mock method
 func (mock *MockUserService) ComparePassword(user *domain.User, password string) error {
 	args := mock.Called(user, password)
 	return args.Error(0)
