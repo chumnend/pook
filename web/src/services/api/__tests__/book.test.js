f2c0237ee51100f00cd8c878d6c0fd196d3848f2
Nicholas Chumney <nicholas.chumney@outlook.com>
2021-07-25 14:48:17 -0400

    fixed book test setup

--- web/src/services/api/__tests__/book.test.js
+++ web/src/services/api/__tests__/book.test.js
@@ -4,8 +4,27 @@ import {} from '../book';
 
 jest.mock('axios');
 
-descibe('list books', () => {});
-descibe('create book', () => {});
-descibe('get book', () => {});
-descibe('update book', () => {});
-descibe('delete book', () => {});
+describe('list books', () => {
+  it.skip('gets list of books', () => {});
+  it.skip('fails to get books', () => {});
+});
+
+describe('create book', () => {
+  it.skip('creates a book', () => {});
+  it.skip('fails to create a book', () => {});
+});
+
+describe('get book', () => {
+  it.skip('gets a book', () => {});
+  it.skip('fails to get a book', () => {});
+});
+
+describe('update book', () => {
+  it.skip('updates a book', () => {});
+  it.skip('fails to update a book', () => {});
+});
+
+describe('delete book', () => {
+  it.skip('deletes a book', () => {});
+  it.skip('fails to delete a book', () => {});
+});
