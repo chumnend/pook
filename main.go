e5c9c43221311cf2ec8d8a4a38d8ef486bf89b8a
Nicholas Chumney <nicholas.chumney@outlook.com>
2022-12-05 20:25:53 -0500

    DEMO: add GET /albums/:id route

--- main.go
+++ main.go
@@ -24,6 +24,7 @@ var albums = []album{
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumByID)
 	router.POST("/albums", postAlbums)
 
 	router.Run("localhost:8080")
@@ -32,6 +33,20 @@ func main() {
 // getAlbums responds with the list of all albums as JSON
 func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
+}
+
+// getAlbumByID locates the album whose ID value matches the id parameter sent by the client
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
 
 // postAlbums adds an album from JSON received in the request body
