e677df580ba09327a007ed726d35cd4810bf0f33
Nicholas Chumney <nicholas.chumney@outlook.com>
2022-12-14 19:39:36 -0500

    setup .env file for pook-api server

--- app/pook-api/webserver/server.go
+++ app/pook-api/webserver/server.go
@@ -1,18 +1,40 @@
 package webserver
 
 import (
+	"log"
+	"os"
+
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 
 	"github.com/chumnend/pook/app/pook-api/routes/album"
 	"github.com/chumnend/pook/app/pook-api/routes/ping"
 )
 
 type Server struct {
-	name   string
-	router *gin.Engine
+	Name   string
+	Router *gin.Engine
+	Port   string
+	Secret string
 }
 
 func New() (*Server, error) {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println(".env file not found")
+		log.Println(err)
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("missing env: PORT")
+	}
+
+	secret := os.Getenv("SECRET_KEY")
+	if secret == "" {
+		log.Fatal("missing env: SECRET_KEY")
+	}
+
 	// setup router
 	router := gin.Default()
 
@@ -22,11 +44,16 @@ func New() (*Server, error) {
 	router.GET("/albums/:id", album.GetByID)
 	router.POST("/albums", album.Post)
 
-	s := &Server{name: "Pook", router: router}
+	s := &Server{
+		Name:   "Pook",
+		Router: router,
+		Port:   port,
+		Secret: secret,
+	}
 
-	return s, nil
+	return s, err
 }
 
 func (s *Server) Start() {
-	s.router.Run("localhost:8080")
+	s.Router.Run(":" + s.Port)
 }
