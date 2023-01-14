70b88257c45d7d6672926503c7b62e389d98c7cd
Nicholas Chumney <nicholas.chumney@outlook.com>
2023-01-13 22:21:03 -0500

    swapped react app to use typescript

--- web/pook-react/src/App.test.tsx
+++ web/pook-react/src/App.test.tsx
@@ -1,9 +1,9 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-
-  const textEl = screen.getByText(/Welcome to Pook!/i);
-  expect(textEl).toBeInTheDocument();
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
