44ff4fad05eba072d30baaa95521f501094d2cbe
Nicholas Chumney <nicholas.chumney@outlook.com>
2025-04-06 21:36:21 -0400

    remove repositories and services

--- go.mod
+++ go.mod
@@ -7,16 +7,4 @@ require (
 	github.com/lib/pq v1.10.9
 )
 
-require (
-	github.com/DATA-DOG/go-sqlmock v1.5.2
-	github.com/dgrijalva/jwt-go v3.2.0+incompatible
-	github.com/joho/godotenv v1.5.1
-	github.com/stretchr/testify v1.10.0
-	golang.org/x/crypto v0.37.0
-)
-
-require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
-)
+require github.com/joho/godotenv v1.5.1
--- internal/users/entity.go
+++ internal/users/entity.go
@@ -1,7 +1,6 @@
 package users
 
 import (
-	"net/http"
 	"time"
 
 	"github.com/google/uuid"
@@ -13,29 +12,4 @@ type User struct {
 	Email        string    `db:"email" json:"email"`
 	PasswordHash string    `db:"password_hash" json:"-"`
 	CreatedAt    time.Time `db:"created_at" json:"createdAt"`
-}
-
-type IUserRepository interface {
-	Create(*User) error
-	FindAll() ([]User, error)
-	FindByID(uuid.UUID) (*User, error)
-	FindByUsername(string) (*User, error)
-	FindByEmail(string) (*User, error)
-	Update(*User) error
-}
-
-type IUserService interface {
-	Create(*User) error
-	FindAll() ([]User, error)
-	FindByUsername(string) (*User, error)
-	FindByEmail(string) (*User, error)
-	Update(*User) error
-	GenerateToken(*User) (string, error)
-	ComparePassword(*User, string) error
-}
-
-type IUserController interface {
-	Register(http.ResponseWriter, *http.Request)
-	Login(http.ResponseWriter, *http.Request)
-	GetUser(http.ResponseWriter, *http.Request)
 }
