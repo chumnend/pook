17a1c8cd40ce43707124f562d47ffbb6ff57952d
Nicholas Chumney <nicholas.chumney@outlook.com>
2025-04-07 08:31:20 -0400

    Revert "remove repositories and services"
    This reverts commit 44ff4fad05eba072d30baaa95521f501094d2cbe.

--- internal/users/entity.go
+++ internal/users/entity.go
@@ -1,6 +1,7 @@
 package users
 
 import (
+	"net/http"
 	"time"
 
 	"github.com/google/uuid"
@@ -12,4 +13,29 @@ type User struct {
 	Email        string    `db:"email" json:"email"`
 	PasswordHash string    `db:"password_hash" json:"-"`
 	CreatedAt    time.Time `db:"created_at" json:"createdAt"`
+}
+
+type IUserRepository interface {
+	Create(*User) error
+	FindAll() ([]User, error)
+	FindByID(uuid.UUID) (*User, error)
+	FindByUsername(string) (*User, error)
+	FindByEmail(string) (*User, error)
+	Update(*User) error
+}
+
+type IUserService interface {
+	Create(*User) error
+	FindAll() ([]User, error)
+	FindByUsername(string) (*User, error)
+	FindByEmail(string) (*User, error)
+	Update(*User) error
+	GenerateToken(*User) (string, error)
+	ComparePassword(*User, string) error
+}
+
+type IUserController interface {
+	Register(http.ResponseWriter, *http.Request)
+	Login(http.ResponseWriter, *http.Request)
+	GetUser(http.ResponseWriter, *http.Request)
 }
