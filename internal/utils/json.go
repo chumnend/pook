20af6133eec427e44a47e07d4f8dddcfcda30069
Nicholas Chumney <nicholas.chumney@outlook.com>
2025-04-12 21:01:57 -0400

    add documnent of test cURL commands

--- internal/utils/json.go
+++ internal/utils/json.go
@@ -3,7 +3,6 @@ package utils
 import (
 	"encoding/json"
 	"net/http"
-	"regexp"
 )
 
 func SendJSON(w http.ResponseWriter, data any, statusCode int) {
@@ -20,9 +19,4 @@ func ParseJSON(r *http.Request, v any) error {
 	decoder := json.NewDecoder(r.Body)
 	decoder.DisallowUnknownFields()
 	return decoder.Decode(v)
-}
-
-func IsValidEmail(email string) bool {
-	re := regexp.MustCompile(`^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$`)
-	return re.MatchString(email)
 }
