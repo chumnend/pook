38b11e9205d3567cbede9ed7d4479788164a4c46
Nicholas Chumney <nicholas.chumney@outlook.com>
2025-06-19 13:18:57 -0400

    remove Page type

--- web/src/helpers/types/Page.ts
+++
@@ -1,9 +0,0 @@
-type Page = {
-    id: string;
-    bookId: string;
-    imageUrl: string;
-    caption: string;
-    pageOrder: number;
-}
-
-export default Page;
--- web/src/helpers/types/index.ts
+++ web/src/helpers/types/index.ts
@@ -1,9 +1,7 @@
 import type Book from "./Book";
-import type Page from "./Page";
 import type User from "./User";
 
 export type {
     Book,
-    Page,
     User,
 };
